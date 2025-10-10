b010d4a7bb6fa82e146ba26e1ae525ada6ea1e2a
Specialist of Everything <88870084+newspecialist@users.noreply.github.com>
2025-10-10 06:23:38 +0000

    Static Deployment

--- Part_2/phonebook/src/Services/persons.js
+++ Part_2/phonebook/src/Services/persons.js
@@ -1,30 +1,29 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+import axios from "axios";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-  const request = axios.get(`${baseUrl}`)
-  return request.then(response => response.data)
-}
+  const request = axios.get(`${baseUrl}`);
+  return request.then((response) => response.data);
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
 
 const doDelete = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(responce => responce.data)
-}
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((responce) => responce.data);
+};
 
-const update = (id, newObject) => {    
-   const request = axios.put(`${baseUrl}/${id}`, newObject)
-   return request.then(responce => responce.data)
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((responce) => responce.data);
+};
 
-}
-
-export default { 
-  getAll: getAll, 
-  create: create, 
+export default {
+  getAll: getAll,
+  create: create,
   update: update,
-  doDelete: doDelete 
-}
+  doDelete: doDelete,
+};
--- Part_3/phonebook/index.js
+++ Part_3/phonebook/index.js
@@ -1,124 +1,119 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.json());
+app.use(express.static("dist"));
 //app.use(morgan(':method :url :response-time'))
 
-app.use(morgan(':method :url :response-time :body', {
-  skip: (req, res) => req.method !== "POST" 
-}));
+app.use(
+  morgan(":method :url :response-time :body", {
+    skip: (req, res) => req.method !== "POST",
+  })
+);
 
-morgan.token('body', function getBody (req) {
-  return JSON.stringify(req.body)
-})
+morgan.token("body", function getBody(req) {
+  return JSON.stringify(req.body);
+});
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
+app.get("/info", (request, response) => {
+  const now = new Date();
 
-app.get('/info', (request, response) => {
-    const now = new Date()
-    
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${now}</p>`)
-})
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${now}</p>`);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const generateId = () => {
-    let min = 0;
-    let max = 99999999999;
-    let random = Math.floor(Math.random() * (+max - +min)) + 
-             +min;
-  return String(random)
-}
+  let min = 0;
+  let max = 99999999999;
+  let random = Math.floor(Math.random() * (+max - +min)) + +min;
+  return String(random);
+};
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
   if (!body.name) {
-    return response.status(422).json({ 
-      error: 'name missing' 
-    })
+    return response.status(422).json({
+      error: "name missing",
+    });
   }
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(409).json({ 
-      error: 'name already exists in the phonebook' 
-    })
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(409).json({
+      error: "name already exists in the phonebook",
+    });
   }
   if (!body.number) {
-    return response.status(422).json({ 
-      error: 'number missing' 
-    })
+    return response.status(422).json({
+      error: "number missing",
+    });
   }
   const person = {
     name: body.name,
     number: body.number,
     id: generateId(),
-  }
+  };
 
-  persons = persons.concat(person)
+  persons = persons.concat(person);
 
-  response.json(person)
-})
+  response.json(person);
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
+app.use(unknownEndpoint);
 
-
-app.use(unknownEndpoint)
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
+  console.log(`Server running on port ${PORT}`);
+});
